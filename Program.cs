16f346067829f9ed0958288dd0c00894ee310018
Ronny Reinhardtsen <ronny@ronnrein.com>
2017-09-26 13:02:29 +0200

    Added command line arguments

--- Program.cs
+++ Program.cs
@@ -1,25 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
-namespace Hiredjs
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+namespace Hiredjs {
+
+    public class Program {
+
+        public static void Main(string[] args) {
             BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        public static IWebHost BuildWebHost(string[] args) {
+            IConfiguration config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().UseConfiguration(config).Build();
+        }
+            
     }
 }
