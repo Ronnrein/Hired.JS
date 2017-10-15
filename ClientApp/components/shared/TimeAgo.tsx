89ff20279cdbc32e5546d0fb1b2df1726146e1b1
Ronny Reinhardtsen <ronny@ronnrein.com>
2017-10-15 13:03:21 +0200

    Fixed timeago bug

--- ClientApp/components/shared/TimeAgo.tsx
+++ ClientApp/components/shared/TimeAgo.tsx
@@ -3,14 +3,23 @@ import { Popup } from "semantic-ui-react";
 import { default as TimeAgoComponent } from "react-timeago";
 
 type Props = {
-    date: Date
+    date: Date;
+    showTooltip?: boolean;
 }
 
 export default class TimeAgo extends React.Component<Props, {}> {
+    public static defaultProps: Partial<Props> = {
+        showTooltip: true
+    };
+
     render() {
+        var el = <TimeAgoComponent date={this.props.date} component="span" title="" />;
+        if(!this.props.showTooltip) {
+            return el;
+        }
         return (
             <Popup
-                trigger={<TimeAgoComponent date={this.props.date} component="span" title="" />}
+                trigger={el}
                 content={new Date(this.props.date).toLocaleString()}
                 size="tiny"
             />
